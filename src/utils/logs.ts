d1c5759f66e25764c54c1f9fc2e664dc71057b1a
Esteban Trillo <esteban@devtrillo.com>
2022-03-09 15:36:56 -0600

    add eslint (#2)
    * Add eslint
    
    * Delete unnesesary file
    
    * Make it eslint compliant

--- src/utils/logs.ts
+++ src/utils/logs.ts
@@ -1,3 +1,10 @@
-export const logGreen = (message: any) => console.log(chalk.green(message));
-export const logRed = (message: any) => console.log(chalk.red(message));
-export const logBlue = (message: any) => console.log(chalk.blue(message));
+import chalk from "chalk";
+type Message = string[] | string;
+
+export const logGreen = (message: Message) =>
+  console.log(chalk.green(...message));
+
+export const logRed = (message: Message) => console.log(chalk.red(...message));
+
+export const logBlue = (message: Message) =>
+  console.log(chalk.blue(...message));
